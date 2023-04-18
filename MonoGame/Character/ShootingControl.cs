f494a90a33a19a47c996bdd22e02d41ee08b1910
JK <maxufimcev094@gmail.com>
2023-04-18 20:42:34 +0500

    Сделана механика стрельбы

---
+++ MonoGame/Character/ShootingControl.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Character;
+
+public class ShootingControl
+{
+    public Texture2D Texture;
+    public Vector2 BulletPath;
+
+    public Texture2D DrawTexture(GraphicsDevice graphicsDevice) =>
+        Texture2D.FromFile(graphicsDevice, "Images/Shooting/1.png");
+    
+    
+    
+}
