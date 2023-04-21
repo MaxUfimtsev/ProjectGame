96c6564d011328394bb0de006cecd6374cfaa555
JK <maxufimcev094@gmail.com>
2023-04-21 20:14:13 +0500

    Сделан рефактор класса персонажа

--- MonoGame/Character/Bullet.cs
+++ MonoGame/Character/Bullet.cs
@@ -3,14 +3,11 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MonoGame.Character;
 
-public class ShootingControl
+public class Bullet
 {
     public Texture2D Texture;
     public Vector2 BulletPath;
 
     public Texture2D DrawTexture(GraphicsDevice graphicsDevice) =>
         Texture2D.FromFile(graphicsDevice, "Images/Shooting/1.png");
-    
-    
-    
 }
